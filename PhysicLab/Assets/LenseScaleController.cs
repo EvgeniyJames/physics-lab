0a731ff6fea9e6eeec5291cb7ace73b37570a6f3
Yevgeniy <evgeniyjames@gmail.com>
2019-05-24 20:14:28 +0300

    +add new Scene +add resources -update something to first lab

---
+++ PhysicLab/Assets/LenseScaleController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LenseScaleController : MonoBehaviour {
+
+    private int step;
+
+    public int stepLimit;
+
+    public float interpolationStep;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Increase()
+    {
+        if (step >= stepLimit)
+            return;
+
+        step++;
+        transform.localScale += new Vector3(0, interpolationStep, 0);
+    }
+
+    public void Decrease()
+    {
+        if (step <= -stepLimit)
+            return;
+
+        step--;
+        transform.localScale -= new Vector3(0, interpolationStep, 0);
+    }
+}
