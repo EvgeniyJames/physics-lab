7549f9a1516ea26d88432990c170e72f5e5235f8
Yevgeniy <evgeniyjames@gmail.com>
2019-05-25 17:31:22 +0300

    +add behavior of change raidus of lense curves

---
+++ PhysicLab/Assets/Script/LenseScaleController.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LenseScaleController : MonoBehaviour {
+
+    private int step;
+
+    public int stepLimit;
+
+    public float interpolationStep;
+
+    public GameObject arrowsContainer;
+
+    private StepIncreaseCoordinator StepIncreaseCoordinator;
+    private StepDecreaseCoordinator StepDecreaseCoordinator;
+
+
+    public Text textWidget;
+    private int curveRadius = 75;
+
+    // Use this for initialization
+    void Start () {
+
+        Debug.Assert(arrowsContainer, "LenseScaleController of " + name + " has no arrowsContainer");
+        if (arrowsContainer)
+        {
+            StepIncreaseCoordinator = arrowsContainer.GetComponent<StepIncreaseCoordinator>();
+            StepDecreaseCoordinator = arrowsContainer.GetComponent<StepDecreaseCoordinator>();
+        }
+        UpdateTextWidget();
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    public void Increase()
+    {
+        if (step >= stepLimit)
+            return;
+
+        step++;
+        transform.localScale -= new Vector3(0, interpolationStep, 0);
+
+        StepDecreaseCoordinator.MakeStep();
+
+        curveRadius += 5;
+        UpdateTextWidget();
+    }
+
+    public void Decrease()
+    {
+        if (step <= -stepLimit)
+            return;
+
+        step--;
+        transform.localScale += new Vector3(0, interpolationStep, 0);
+
+        StepIncreaseCoordinator.MakeStep();
+
+        curveRadius -= 5;
+        UpdateTextWidget();
+    }
+
+    private void UpdateTextWidget()
+    {
+        if (textWidget)
+            textWidget.text = "" + curveRadius;
+    }
+}
---
+++ PhysicLab/Assets/Script/StepDecreaseCoordinator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class StepDecreaseCoordinator : MonoBehaviour {
+
+    public enum SIDE { UP, LEFT, DEPTH};
+    public SIDE side;
+
+    public float step;
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MakeStep()
+    {
+
+        if(side == SIDE.UP)
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x, transform.localPosition.y - step, transform.localPosition.z);
+        }
+    }
+}
