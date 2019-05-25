548f4fc30b46db6b294555c24b8aa18dc69c3a31
Yevgeniy <evgeniyjames@gmail.com>
2019-05-25 20:56:11 +0300

    +add labmda to main screen

--- PhysicLab/Assets/Script/ArrowColorifyier.cs
+++ PhysicLab/Assets/Script/ArrowColorifyier.cs
@@ -1,20 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class ArrowColorifyier : MonoBehaviour {
 
+    public struct ColorButtonParams
+    {
+        public Color color;
+        public int lambda { get; set; }
+    }
+
     public GameObject arrowContainer;
+
+    public Text textLambda;
+
+    private ColorButtonParams[] paramsCache;
 
 	// Use this for initialization
 	void Start () {
-        for(int i = 0; i < transform.childCount; i++)
+
+        paramsCache = new ColorButtonParams[transform.childCount];
+
+        for (int i = 0; i < transform.childCount; i++)
         {
+            ColorButtonParams colorButtonParams = new ColorButtonParams();
+
             Button button = transform.GetChild(i).GetComponent<Button>();
-            button.onClick.AddListener(() => ColorArrows(button.GetComponent<Image>().color));
+
+            colorButtonParams.color = button.GetComponent<Image>().color;
+            colorButtonParams.lambda = button.GetComponent<ArrowColorParams>().lambdaValue;
+
+            button.onClick.AddListener(() => ColorArrows(colorButtonParams));
+            paramsCache[i] = colorButtonParams;
         }
-        Debug.Log(transform.childCount);
+
+        ColorArrows(paramsCache[Random.Range(0, paramsCache.Length)]);
 	}
 	
 	// Update is called once per frame
@@ -22,19 +41,22 @@ public class ArrowColorifyier : MonoBehaviour {
 		
 	}
 
-    void ColorArrows(Color color)
+    void ColorArrows(ColorButtonParams colorButtonParams)
     {
         for(int i = 0; i < arrowContainer.transform.childCount; i++)
         {
             SpriteRenderer spriteRenderer = arrowContainer.transform.GetChild(i).GetComponent<SpriteRenderer>();
             if(spriteRenderer)
             {
-                spriteRenderer.color = color;
+                spriteRenderer.color = colorButtonParams.color;
             }
             else
             {
-                arrowContainer.transform.GetChild(i).GetChild(0).GetComponent<SpriteRenderer>().color = color;
+                arrowContainer.transform.GetChild(i).GetChild(0).GetComponent<SpriteRenderer>().color = colorButtonParams.color;
             }
         }
+
+        if (textLambda)
+            textLambda.text = "" + colorButtonParams.lambda;
     }
 }
