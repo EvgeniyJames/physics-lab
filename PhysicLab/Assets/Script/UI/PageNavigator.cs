81029525e5a1148e855d496fdcd20fd1aac8ead4
Yevgeniy <evgeniyjames@gmail.com>
2019-05-28 23:06:39 +0300

    -update UI

---
+++ PhysicLab/Assets/Script/UI/PageNavigator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PageNavigator : MonoBehaviour {
+
+    public RectTransform nextPanel; 
+    
+    public void OnNextPanelPressed()
+    {
+        RectTransform rectTransform = transform.parent.transform as RectTransform;
+        rectTransform.Translate(new Vector2(-Screen.width, 0));
+
+        nextPanel.Translate(new Vector2(-Screen.width, 0));
+    }
+}
---
+++ PhysicLab/Assets/Script/UI/SceneNavigator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneNavigator : MonoBehaviour {
+
+    public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Button>().onClick.AddListener(OnNewScenePressed);
+	}
+
+    void OnNewScenePressed()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
