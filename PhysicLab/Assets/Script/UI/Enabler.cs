52594a3bcc75aa2b1c15f6ae7db3342a07180be1
Yevgeniy <evgeniyjames@gmail.com>
2019-06-09 21:03:58 +0300

    +add ruler and grid to Newton's Circles

---
+++ PhysicLab/Assets/Script/UI/Enabler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class Enabler : MonoBehaviour
+{
+    public GameObject toEnable;
+
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(OnEnable);
+    }
+
+    public void OnEnable()
+    {
+        if (toEnable)
+            toEnable.SetActive(!toEnable.activeSelf);
+    }
+}
