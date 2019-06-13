2f05e40136fbb1c3292483972e9f54cd1aa72d6e
Yevgeniy <evgeniyjames@gmail.com>
2019-06-13 18:34:02 +0300

    +add DrawCircle

---
+++ PhysicLab/Assets/Script/Utils/DrawCircle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class DrawCircle : MonoBehaviour
+{
+    public float radius;
+    public float lineWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.DrawCircle(radius, lineWidth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
