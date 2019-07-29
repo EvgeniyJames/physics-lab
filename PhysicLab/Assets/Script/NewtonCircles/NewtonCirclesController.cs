b1d8457ab32c99315d2ff91941e7a2f3ca5d9f4e
Yevgeniy <evgeniyjames@gmail.com>
2019-07-29 19:29:38 +0300

    +add redirection from scene to scene

---
+++ PhysicLab/Assets/Script/NewtonCircles/NewtonCirclesController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewtonCirclesController : MonoBehaviour
+{
+    public GameObject canvas;
+    public GameObject world;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas.SetActive(false);
+        world.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PhysicLab/Assets/Script/UI/OnSceneObjectPressEnablerDisabler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider))]
+public class OnSceneObjectPressEnablerDisabler : MonoBehaviour
+{
+    public GameObject toEnable;
+    public GameObject toDisable;
+
+    private void OnMouseDown()
+    {
+        OnPressed();
+    }
+
+    public void OnPressed()
+    {
+        toEnable.SetActive(true);
+
+        if (toDisable)
+            toDisable.SetActive(false);
+        else
+            gameObject.SetActive(false);
+    }
+}
