fc45cd10da290cf24012a4a18a089892d43e7eae
Yevgeniy <evgeniyjames@gmail.com>
2019-08-05 18:27:25 +0300

    +add forgotten class

---
+++ PhysicLab/Assets/Script/UI/ToogleScaler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ToogleScaler : MonoBehaviour
+{
+    public GameObject scaleObject;
+    public float scaleTo;
+
+    private bool isScaled;
+
+    private Vector3 startScale;
+    private Vector3 newScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startScale = (scaleObject.transform.localScale);
+        newScale = new Vector3(scaleTo, scaleTo, scaleTo);
+
+        GetComponent<Button>().onClick.AddListener(OnScaleToogle);
+    }
+
+    public void OnScaleToogle()
+    {
+        if (isScaled)
+            scaleObject.GetComponent<RectTransform>().localScale = startScale;
+        else
+            scaleObject.GetComponent<RectTransform>().localScale = newScale;
+
+        Canvas.ForceUpdateCanvases();
+
+        isScaled = !isScaled;
+    }
+}
---
+++ PhysicLab/Assets/Script/Utils/DrawRuler.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class DrawRuler : MonoBehaviour
+{
+    public int counter;
+    public Color color;
+
+    public GameObject prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    public void Draw()
+    {
+        Clear();
+
+        for (int i = 0; i < 10; i++)
+        {
+            var newLine = Instantiate(prefab, transform);
+            AddToObject(newLine, i, color);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void Init()
+    {
+        gameObject.DrawRuler(counter, color);
+    }
+
+    public static void AddToObject(GameObject where, int counter, Color color)
+    {
+        DrawRuler drawCircle = where.GetComponent<DrawRuler>();
+
+        if (!drawCircle)
+            drawCircle = where.AddComponent<DrawRuler>();
+
+        drawCircle.counter = counter;
+        drawCircle.color = color;
+        drawCircle.Init();
+    }
+
+    private void Clear()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject.GetComponent<LineRenderer>().material);
+
+            Destroy(child.gameObject);
+        }
+    }
+}
